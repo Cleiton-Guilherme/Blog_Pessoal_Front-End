fed0f90bdb806d0f9a118a9f3d6659e079606917
Cleiton-Guilherme <Cleiton.guilherme@yahoo.com.br>
2022-08-30 16:53:29 -0300

    Sistemas De Rotas Blog-pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// SELECT name As Noe da p. Usuaria FROM USERS
+
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
+import Login from './paginas/Login/Login';
+import './App.css';
 
 function App() {
   return (
-    <>
-     <Navbar />
-     <Home />
-     <Footer />
-    </>
-    
+
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+
+
   );
 }
 
