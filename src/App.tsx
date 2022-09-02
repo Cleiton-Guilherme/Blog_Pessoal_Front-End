64c810df8a681cfc2e3835b14340c09cc51a9135
Cleiton-Guilherme <Cleiton.guilherme@yahoo.com.br>
2022-09-01 21:46:18 -0300

    Comunicando Back-End é Front-End

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import Login from './paginas/Login/Login';
 import './App.css';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
   return (
@@ -15,10 +16,15 @@ function App() {
     <Router>
       <Navbar />
       <div style={{ minHeight: '100vh' }}>
+
         <Routes> // Antigo Switch
+
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
+
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+
         </Routes>
       </div>
       <Footer />
